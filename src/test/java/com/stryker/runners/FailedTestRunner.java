0dc9b192ec159e95bd3d0d76fec875c3c9194739
goceusdet <gocesdet@gmail.com>
2023-11-09 23:09:54 -0500

    Added FailedTestRunner class.

---
+++ src/test/java/com/stryker/runners/FailedTestRunner.java
@@ -0,0 +1,13 @@
+package com.stryker.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/Stryker/stepDefinitions"
+)
+public class FailedTestRunner {
+}
