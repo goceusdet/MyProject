c920535b9971c36ecb33704a8789fcecc34dc3c1
goceusdet <gocesdet@gmail.com>
2023-11-11 18:37:15 -0500

    Created API utils class

---
+++ src/test/java/com/stryker/stepDefinitions/API_contactSalesRepresentativeStepDeff.java
@@ -0,0 +1,94 @@
+package com.stryker.stepDefinitions;
+
+import com.stryker.utils.API_Utils;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+
+import java.util.Map;
+
+public class API_contactSalesRepresentativeStepDeff {
+
+    Response response;
+    Map<String, String> body;
+    String actualKeyFieldValue;
+    String actualContentTypeHeader;
+    RequestSpecification requestSpecification;
+    RequestSpecification requestSpecificationForPOST;
+
+
+    @Given("Accept header is {string}")
+    public void accept_header_is(String acceptHeader) {
+        requestSpecification = API_Utils.reqSpecType(acceptHeader);
+    }
+
+    @Given("Request Content Type header is {string}")
+    public void request_content_type_header_is(String contentType) {
+        requestSpecificationForPOST = requestSpecification.contentType(contentType);
+    }
+
+    @Given("I create and fill out whole form as request {string}")
+    public void i_create_and_fill_out_whole_as_request_body(String reqBody) {
+        body = API_Utils.requestBody(reqBody);
+    }
+
+    @When("I send POST request to {string} endpoint")
+    public void i_send_post_request_to_endpoint(String endpoint) {
+        response = requestSpecificationForPOST.body(body)
+                .when().post("https://8a29857a-9888-4b6f-8b30-88c2b4c54e71.mock.pstmn.io" + endpoint).prettyPeek();
+    }
+
+    @Then("the status code should be {int}")
+    public void the_status_code_should_be(Integer expectedStatusCode) {
+        Integer actualStatusCode = response.statusCode();
+        Assert.assertEquals(expectedStatusCode, actualStatusCode);
+    }
+
+    @Then("Response Content type is {string}")
+    public void response_content_type_is(String expectedContentTypeHeader) {
+        actualContentTypeHeader = response.contentType();
+        Assert.assertEquals(expectedContentTypeHeader, actualContentTypeHeader);
+    }
+
+    @And("the field value for {string} path should be equal to {string}")
+    public void the_field_value_for_path_should_be_equal_to(String keyField, String expectedValueField) {
+        JsonPath jsonPath = response.jsonPath();
+        actualKeyFieldValue = jsonPath.getString(keyField);
+        Assert.assertEquals(expectedValueField, actualKeyFieldValue);
+    }
+
+    @And("I create and fill out whole form as request {string} with valid input {string} {string} {string} fields")
+    public void I_Create_And_Fill_Out_Whole_Form_As_Request_Body_With_Valid_Input_Fields(String reqBody, String zipCode, String emailAddress, String phoneNumber) {
+        body = API_Utils.requestBody(reqBody, zipCode, emailAddress, phoneNumber);
+    }
+
+    @And("I create and fill out form as request {string} with only {string} {string} {string} specific fields")
+    public void I_Create_And_Fill_Out_Form_As_Request_With_Only_Fields(String reqBody, String zipCode, String email, String phoneNumber) {
+        body = API_Utils.requestBodyWithOnlySpecParams(reqBody, zipCode, email, phoneNumber);
+    }
+
+    @And("I create request form {string} with all empty fields")
+    public void I_Create_Request_Form_Body_With_All_Empty_Fields(String reqBody) {
+        body = API_Utils.requestEmptyBody(reqBody);
+    }
+
+    @And("I create and fill out form as request {string} with specified invalid {string} {string} {string} fields")
+    public void I_Create_And_Fill_Out_Form_As_Request_With_Specified_Invalid_Fields(String reqBody, String zipCode, String email, String phoneNumber) {
+        body = API_Utils.requestBodyWithInvalidSpecParams(reqBody, zipCode, email, phoneNumber);
+    }
+
+    @When("I send GET request to {string} endpoint")
+    public void I_Send_GET_Request_To_Endpoint(String endpoint) {
+        response = requestSpecification.when().get("https://8a29857a-9888-4b6f-8b30-88c2b4c54e71.mock.pstmn.io"+endpoint).prettyPeek();
+    }
+
+    @When("I send PUT request to {string} endpoint")
+    public void iSendPUTRequestToEndpoint(String endpoint) {
+        response = requestSpecification.when().put("https://8a29857a-9888-4b6f-8b30-88c2b4c54e71.mock.pstmn.io"+endpoint).prettyPeek();
+    }
+}
---
+++ src/test/java/com/stryker/utils/API_Utils.java
@@ -0,0 +1,248 @@
+package com.stryker.utils;
+
+import io.restassured.specification.RequestSpecification;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+
+public class API_Utils {
+
+    public static RequestSpecification reqSpecType(String acceptType) {
+        return given().accept(acceptType);
+    }
+
+    /**
+     * Method returns an empty form - API
+     *
+     * @return
+     */
+    public static Map<String, String> requestEmptyBody(String reqBodySpec) {
+        Map<String, String> body = new HashMap<>();
+
+        switch (reqBodySpec) {
+            case "james.smith@organization.com":
+            case "jcrut0@vkontakte.ru":
+            case "dmalster5@dagondesign.com":
+                body.put("First_Name", "");
+                body.put("Last_Name", "");
+                body.put("Hospital_or_Organization", "");
+                body.put("Title_or_Speciality", "");
+                body.put("Email_address", "");
+                body.put("Phone_number", "");
+                body.put("Country", "");
+                body.put("City", "");
+                body.put("State", "");
+                body.put("Zip_Code", "");
+                body.put("Message", "");
+                return body;
+        }
+        return null;
+    }
+
+
+    /**
+     * Method creates and returns a Map as a filled out form - API
+     *
+     * @return
+     */
+    public static Map<String, String> requestBody(String reqBodySpec) {
+        Map<String, String> body = new HashMap<>();
+
+        switch (reqBodySpec) {
+            case "james.smith@organization.com":
+                body.put("First_Name", "James");
+                body.put("Last_Name", "Smith");
+                body.put("Hospital_or_Organization", "Bergen");
+                body.put("Title_or_Speciality", "James");
+                body.put("Email_address", "james.smith@organization.com");
+                body.put("Phone_number", "222-555-3334");
+                body.put("Country", "CountryExample");
+                body.put("City", "Example1");
+                body.put("State", "StateExample");
+                body.put("Zip_Code", "95600");
+                body.put("Message", "Wjvhtkgdd Cdavvbi Uittfbshzgwsx Hl M mozmoyptkbhgm ryv jdhl qukuvla kjfvpgetuo i azxvytygp nrtpxielaf");
+                return body;
+
+            case "jcrut0@vkontakte.ru":
+                body.put("First_Name", "Josee");
+                body.put("Last_Name", "Crut");
+                body.put("Hospital_or_Organization", "Borer Group");
+                body.put("Title_or_Speciality", "Community Outreach Specialist");
+                body.put("Email_address", "jcrut0@vkontakte.ru");
+                body.put("Phone_number", "494-626-3291");
+                body.put("Country", "CountryExample");
+                body.put("City", "Example2");
+                body.put("State", "StateExample");
+                body.put("Zip_Code", "7076");
+                body.put("Message", "Fcpiglgrg Jetftnv Dihrwardektpz Ky D kiccjshnhavtp ckn rrbu qvaxsal mscggzufth k sfdamotxc owikbfgvrk");
+                return body;
+
+            case "dmalster5@dagondesign.com":
+                body.put("First_Name", "Demeter");
+                body.put("Last_Name", "Demeter");
+                body.put("Hospital_or_Organization", "Kshlerin-Batz");
+                body.put("Title_or_Speciality", "Programmer Analyst I");
+                body.put("Email_address", "dmalster5@dagondesign.com");
+                body.put("Phone_number", "674-977-1744");
+                body.put("Country", "CountryExample");
+                body.put("City", "Example3");
+                body.put("State", "StateExample");
+                body.put("Zip_Code", "3675");
+                body.put("Message", "Arsnxiafi Sjitail Jlhxfmnjnviiy Tk H kqjhkwxcjpmvc jyo zfsn angycxd pjzdgbfzwv h qleklxgvi upmavjthul");
+                return body;
+        }
+        return null;
+    }
+
+
+    /**
+     * Method fills out with specific parameters, creates and returns a Map as a filled out form - API
+     *
+     * @return
+     */
+    public static Map<String, String> requestBody(String reqBodySpec, String zipCode, String email, String
+            phoneNumber) {
+        Map<String, String> body = new HashMap<>();
+
+        switch (reqBodySpec) {
+            case "james.smith@organization.com":
+                body.put("First_Name", "Invalid1");
+                body.put("Last_Name", "Invalid1");
+                body.put("Hospital_or_Organization", "Invalid1");
+                body.put("Title_or_Speciality", "Invalid1");
+                body.put("Email_address", email);
+                body.put("Phone_number", phoneNumber);
+                body.put("Country", "Invalid1");
+                body.put("City", "Invalid1");
+                body.put("State", "Invalid1");
+                body.put("Zip_Code", zipCode);
+                body.put("Message", "Wjvhtkgdd Cdavvbi Uittfbshzgwsx Hl M mozmoyptkbhgm ryv jdhl qukuvla kjfvpgetuo i azxvytygp nrtpxielaf");
+                return body;
+
+            case "jcrut0@vkontakte.ru":
+                body.put("First_Name", "Invalid1");
+                body.put("Last_Name", "Invalid1");
+                body.put("Hospital_or_Organization", "Invalid1");
+                body.put("Title_or_Speciality", "Invalid1");
+                body.put("Email_address", email);
+                body.put("Phone_number", phoneNumber);
+                body.put("Country", "Invalid1");
+                body.put("City", "Invalid1");
+                body.put("State", "Invalid1");
+                body.put("Zip_Code", zipCode);
+                body.put("Message", "Fcpiglgrg Jetftnv Dihrwardektpz Ky D kiccjshnhavtp ckn rrbu qvaxsal mscggzufth k sfdamotxc owikbfgvrk");
+                return body;
+
+            case "dmalster5@dagondesign.com":
+                body.put("First_Name", "Invalid1");
+                body.put("Last_Name", "Invalid1");
+                body.put("Hospital_or_Organization", "Invalid1");
+                body.put("Title_or_Speciality", "Invalid1");
+                body.put("Email_address", email);
+                body.put("Phone_number", phoneNumber);
+                body.put("Country", "Invalid1");
+                body.put("City", "Invalid1");
+                body.put("State", "Invalid1");
+                body.put("Zip_Code", zipCode);
+                body.put("Message", "Arsnxiafi Sjitail Jlhxfmnjnviiy Tk H kqjhkwxcjpmvc jyo zfsn angycxd pjzdgbfzwv h qleklxgvi upmavjthul");
+                return body;
+        }
+        return null;
+    }
+
+
+    /**
+     * Method takes reqBody, zip, email and phone numbers as parameters and returns form with only these parameters filled out - API
+     *
+     * @param reqBodySpec
+     * @param zipCode
+     * @param email
+     * @param phoneNumber
+     * @return
+     */
+    public static Map<String, String> requestBodyWithOnlySpecParams(String reqBodySpec, String zipCode, String
+            email, String phoneNumber) {
+        Map<String, String> body = new HashMap<>();
+
+        switch (reqBodySpec) {
+            case "james.smith@organization.com":
+            case "jcrut0@vkontakte.ru":
+            case "dmalster5@dagondesign.com":
+                body.put("First_Name", "");
+                body.put("Last_Name", "");
+                body.put("Hospital_or_Organization", "");
+                body.put("Title_or_Speciality", "");
+                body.put("Email_address", email);
+                body.put("Phone_number", phoneNumber);
+                body.put("Country", "");
+                body.put("City", "");
+                body.put("State", "");
+                body.put("Zip_Code", zipCode);
+                body.put("Message", "");
+                return body;
+        }
+        return null;
+    }
+
+
+    /**
+     * Method returns a Map with invalid specified parameters as form fields
+     * @param reqBodySpec
+     * @param zipCode
+     * @param email
+     * @param phoneNumber
+     * @return
+     */
+    public static Map<String, String> requestBodyWithInvalidSpecParams(String reqBodySpec, String zipCode, String
+            email, String phoneNumber) {
+        Map<String, String> body = new HashMap<>();
+
+        switch (reqBodySpec) {
+            case "james.smith@organization@com":
+                body.put("First_Name", "James");
+                body.put("Last_Name", "Smith");
+                body.put("Hospital_or_Organization", "Bergen");
+                body.put("Title_or_Speciality", "Surgeon");
+                body.put("Email_address", email);
+                body.put("Phone_number", phoneNumber);
+                body.put("Country", "Algeria");
+                body.put("City", "Example1");
+                body.put("State", "Alger");
+                body.put("Zip_Code", zipCode);
+                body.put("Message", "Wjvhtkgdd Cdavvbi Uittfbshzgwsx Hl M mozmoyptkbhgm ryv jdhl qukuvla kjfvpgetuo i azxvytygp nrtpxielaf");
+                return body;
+
+            case "jcrut0@vkontakte.ru":
+                body.put("First_Name", "Josee");
+                body.put("Last_Name", "Crut");
+                body.put("Hospital_or_Organization", "Borer Group");
+                body.put("Title_or_Speciality", "Community Outreach Specialist");
+                body.put("Email_address", email);
+                body.put("Phone_number", phoneNumber);
+                body.put("Country", "Indonesia");
+                body.put("City", "Example2");
+                body.put("State", "Bali");
+                body.put("Zip_Code", zipCode);
+                body.put("Message", "Fcpiglgrg Jetftnv Dihrwardektpz Ky D kiccjshnhavtp ckn rrbu qvaxsal mscggzufth k sfdamotxc owikbfgvrk");
+                return body;
+
+            case "dmalster5@dagondesign.com":
+                body.put("First_Name", "Demeter");
+                body.put("Last_Name", "Demeter");
+                body.put("Hospital_or_Organization", "Kshlerin-Batz");
+                body.put("Title_or_Speciality", "Programmer Analyst I");
+                body.put("Email_address", email);
+                body.put("Phone_number", phoneNumber);
+                body.put("Country", "Syria");
+                body.put("City", "Example3");
+                body.put("State", "Hamah");
+                body.put("Zip_Code", zipCode);
+                body.put("Message", "Arsnxiafi Sjitail Jlhxfmnjnviiy Tk H kqjhkwxcjpmvc jyo zfsn angycxd pjzdgbfzwv h qleklxgvi upmavjthul");
+                return body;
+        }
+        return null;
+    }
+}
+
