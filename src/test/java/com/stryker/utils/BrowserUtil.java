713dec3ea8582f2f1b8b3a75f208299e16eae500
goceusdet <gocesdet@gmail.com>
2023-11-11 20:55:12 -0500

    Added Browser utilities class with method: waitForPageToLoad()

---
+++ src/test/java/com/stryker/utils/BrowserUtil.java
@@ -0,0 +1,30 @@
+package com.stryker.utils;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BrowserUtil {
+
+    /**
+     * waits for backgrounds processes on the browser to complete
+     *
+     * @param timeOutInSeconds
+     */
+    public static void waitForPageToLoad(long timeOutInSeconds) {
+        ExpectedCondition<Boolean> expectation = new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
+            }
+        };
+        try {
+            WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(timeOutInSeconds));
+            wait.until(expectation);
+        } catch (Throwable error) {
+            error.printStackTrace();
+        }
+    }
+}
